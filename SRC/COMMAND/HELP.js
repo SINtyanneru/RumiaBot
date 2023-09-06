08a5913fa527d0c1da60d086565147eba4ab93e5
るみさん <irjitgd@gmail.com>
2023-09-06 15:20:40 +0900

    漢字変換

--- SRC/COMMAND/HELP.js
+++ SRC/COMMAND/HELP.js
@@ -8,55 +8,55 @@ class HELP{
 		const CMD = E.options.getString("mode");
 		if(CMD === "slash"){
 			const EB = new MessageEmbed();
-			EB.setTitle("るみさんBOTのヘルプ");
-			EB.setDescription("スラッシュコマンド");
+			EB.setTitle("RumiBOT help");
+			EB.setDescription("Sla¡sh komand");
 			EB.setColor(RND_COLOR());
 	
 			EB.addFields({
 				name: "/ping",
-				value: "pignをします、\n使用方法：/ping host:pingする先",
+				value: "Ти а Ping ӱи эру;\nЭрйлйки：/ping host:Hosto Neymu",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/test",
-				value: "テストで作った恒例のコマンドです、\n使用方法：/test",
+				value: "Ти а testo komand;\nЭрйлйки：/test",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/ferris",
-				value: "フェリスの画像を出します、\n使用方法：/ferris type:フェリスのタイプ",
+				value: "Ти а Feris ӱи komand;\nЭрйлйки：/ferris type:Ferris ӱу форма",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/ws",
-				value: "ウェブサイトをスクショする、\n使用方法：/ws url:スクショ先のURL、httpsを省ける",
+				value: "Ти а WebuSayto ӱи Suksho komand\nЭрйлйки：/ws url:スクショ先のURL、httpsを省ける",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/info_server",
-				value: "よく有るやつ、鯖の情報を取ります、\n使用方法：/info_server",
+				value: "よく有るやつ、鯖の情報を取ります、\nЭрйлйки：/info_server",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/info_user",
-				value: "よく有るやつ、ユーザーの情報を取ります、\n使用方法：/info_user",
+				value: "よく有るやつ、ユーザーの情報を取ります、\nЭрйлйки：/info_user",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/kanji",
-				value: "漢字の変換ができます、\n使用方法：/kanji text:漢字が含まれた文字列 mode:変換方法",
+				value: "漢字の変換ができます、\nЭрйлйки：/kanji text:漢字が含まれた文字列 mode:変換方法",
 				inline: false
 			});
 
 			EB.addFields({
 				name: "/letter",
-				value: "文字の変換ができます、\n使用方法：/letter text:文字列 old:元の文字 new:変換後の文字",
+				value: "文字の変換ができます、\nЭрйлйки：/letter text:文字列 old:元の文字 new:変換後の文字",
 				inline: false
 			});
 
@@ -69,7 +69,7 @@ class HELP{
 	
 			EB.addFields({
 				name: "検索",
-				value: "ググります、\n使用方法：検索 検索キーワード",
+				value: "ググります、\nЭрйлйки：検索 検索キーワード",
 				inline: false
 			});
 
