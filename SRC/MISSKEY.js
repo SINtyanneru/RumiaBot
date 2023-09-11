736d10ebd6987d9b9433c41a34b16f7c8cb59e44
るみさん <irjitgd@gmail.com>
2023-09-11 14:07:42 +0900

    Misskeyがエラーで落ちないように

--- SRC/MISSKEY.js
+++ SRC/MISSKEY.js
@@ -39,80 +39,85 @@ class MISSKEY{
 
 		//サーバーからメッセージを受信した際のイベントハンドラ
 		socket.on('message', (DATA) => {
-			const RESULT = JSON.parse(DATA);
-			if(RESULT.body.type === "note"){
-				let IT_MIS_USER = RESULT.body.body.user;
-				let IT_DIS_USER = USER[IT_MIS_USER.id];
-				let NOTE_TEXT = RESULT.body.body.text;
-				let NOTE_FILES = RESULT.body.body.files;
-				let NOTE_ID = RESULT.body.body.id;
-				let RENOTE_ID = RESULT.body.body.renoteId;
-				let RENOTE_NOTE = RESULT.body.body.renote;
-				
-				if(IT_DIS_USER !== undefined){
-					console.log("[ INFO ][ MISSKEY ]Note res:" +NOTE_ID);//ログを吐く
-
-					const EB = new MessageEmbed();
-					//ユーザー名
-					EB.setTitle(IT_MIS_USER.name);
-
-					//本文
-					if(NOTE_TEXT !== undefined && NOTE_TEXT !== null){//本文が有るか
-						//ある
-						EB.setDescription(NOTE_TEXT);
-					}
-
-					//色
-					EB.setColor(RND_COLOR());
-
-					//URL
-					EB.setURL("https://ussr.rumiserver.com/@" + IT_MIS_USER.id);
-
-					if(NOTE_FILES[0] !== undefined && NOTE_FILES[0] !== null){
-						if(!RENOTE_NOTE.files[0].isSensitive){
-							EB.setImage(NOTE_FILES[0].thumbnailUrl);
+			try {
+				const RESULT = JSON.parse(DATA);
+				if(RESULT.body.type === "note"){
+					let IT_MIS_USER = RESULT.body.body.user;
+					let IT_DIS_USER = USER[IT_MIS_USER.id];
+					let NOTE_TEXT = RESULT.body.body.text;
+					let NOTE_FILES = RESULT.body.body.files;
+					let NOTE_ID = RESULT.body.body.id;
+					let RENOTE_ID = RESULT.body.body.renoteId;
+					let RENOTE_NOTE = RESULT.body.body.renote;
+					
+					if(IT_DIS_USER !== undefined){
+						console.log("[ INFO ][ MISSKEY ]Note res:" + NOTE_ID);//ログを吐く
+	
+						const EB = new MessageEmbed();
+						//ユーザー名
+						EB.setTitle(IT_MIS_USER.name);
+	
+						//本文
+						if(NOTE_TEXT !== undefined && NOTE_TEXT !== null){//本文が有るか
+							//ある
+							EB.setDescription(NOTE_TEXT);
 						}
-					}
-
-					//リノート関連
-					if(RENOTE_ID !== null && RENOTE_ID !== undefined){//リノートはあるか
-						//あるのでリノート元を貼る
-						if(RENOTE_NOTE.text !== undefined && RENOTE_NOTE.text !== null){
-							EB.addFields({
-								name: "リノート元\n" + RENOTE_NOTE.user.name,
-								value: RENOTE_NOTE.text,
-								inline: false
-							});
-						}else{
-							EB.addFields({
-								name: "リノート元\n" + RENOTE_NOTE.user.name,
-								value: "[テキストナシ]",
-								inline: false
-							});
+	
+						//色
+						EB.setColor(RND_COLOR());
+	
+						//URL
+						EB.setURL("https://ussr.rumiserver.com/@" + IT_MIS_USER.id);
+	
+						if(NOTE_FILES !== 0){
+							if(!NOTE_FILES[0].isSensitive){
+								EB.setImage(NOTE_FILES[0].thumbnailUrl);
+							}
 						}
-						
-						//リノートじの画像
-						if(NOTE_FILES[0] === undefined){//既に画像が有るか
-							//リノート元に画像は有るか
-							if(RENOTE_NOTE.files[0] !== undefined && RENOTE_NOTE.files[0] !== null){
-								if(!RENOTE_NOTE.files[0].isSensitive){
-									EB.setImage(RENOTE_NOTE.files[0].thumbnailUrl);
+	
+						//リノート関連
+						if(RENOTE_ID !== null){//リノートはあるか
+							//あるのでリノート元を貼る
+							if(RENOTE_NOTE.text !== undefined && RENOTE_NOTE.text !== null && RENOTE_NOTE.text !== ""){
+								EB.addFields({
+									name: "リノート元\n" + RENOTE_NOTE.user.name,
+									value: RENOTE_NOTE.text,
+									inline: false
+								});
+							}else{
+								EB.addFields({
+									name: "リノート元\n" + RENOTE_NOTE.user.name,
+									value: "[テキストナシ]",
+									inline: false
+								});
+							}
+							
+							//リノートじの画像
+							if(NOTE_FILES === 0){//既に画像が有るか
+								//リノート元に画像は有るか
+								if(RENOTE_NOTE.files !== 0){
+									if(!RENOTE_NOTE.files[0].isSensitive){
+										EB.setImage(RENOTE_NOTE.files[0].thumbnailUrl);
+									}
 								}
 							}
 						}
+	
+						//アクション
+						EB.addFields({
+							name: "ｱクション",
+							value: "[見に行く](https://ussr.rumiserver.com/notes/" + NOTE_ID +")|"+
+									"[何もしない](https://google.com)",
+							inline: false
+						});
+	
+						//そのまま送りつける
+						MSG_SEND(IT_DIS_USER.GID, IT_DIS_USER.CID, { embeds: [EB] });
 					}
-
-					//アクション
-					EB.addFields({
-						name: "ｱクション",
-						value: "[見に行く](https://ussr.rumiserver.com/notes/" + NOTE_ID +")|"+
-								"[何もしない](https://google.com)",
-						inline: false
-					});
-
-					//そのまま送りつける
-					MSG_SEND(IT_DIS_USER.GID, IT_DIS_USER.CID, { embeds: [EB] });
 				}
+			}catch(EX){
+				console.log("[ ERR ][ MISSKEY ]" + EX);
+				return;
 			}
 		});
 
