c3faec7f85341920de1ba0a3613fc1d1115020bc
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-09-12 21:22:41 +0900

    ただの整形

--- SRC/MODULES/RUMI_HAPPY_BIRTHDAY.js
+++ SRC/MODULES/RUMI_HAPPY_BIRTHDAY.js
@@ -1,19 +1,19 @@
-export function RUMI_HAPPY_BIRTHDAY(){
+export function RUMI_HAPPY_BIRTHDAY() {
 	//2007年10月29日の日付を作成
 	const targetDate = new Date(2007, 9, 29); //月は0から始まるため、9は10月を表す
-		
+
 	//今日の日付を取得
 	const today = new Date();
-		
+
 	//年数の差を計算
 	let yearDifference = today.getFullYear() - targetDate.getFullYear();
-		
+
 	//10月29日以前の場合、1年引く
-	if(
+	if (
 		today.getMonth() < targetDate.getMonth() ||
 		(today.getMonth() === targetDate.getMonth() &&
-		today.getDate() < targetDate.getDate())
-	){
+			today.getDate() < targetDate.getDate())
+	) {
 		yearDifference--;
 	}
 	return yearDifference;
