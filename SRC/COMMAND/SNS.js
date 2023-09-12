409a911b905356ebe761c6f000e631bfb61c4a8c
るみさん <irjitgd@gmail.com>
2023-09-12 23:02:22 +0900

    SNS:ユーザー情報を取得

--- SRC/COMMAND/SNS.js
+++ SRC/COMMAND/SNS.js
@@ -7,6 +7,7 @@ export class SNS {
 	async main() {
 		let E = this.E;
 		const TYPE = E.options.getString("type");
+		const USER_NAME = E.options.getString("userid");
 
 		//インスタンスの設定を取得
 		let SNS_CONFIG = CONFIG.SNS.find((ROW) => ROW.ID === TYPE);
@@ -14,8 +15,30 @@ export class SNS {
 		//設定があるか
 		if (SNS_CONFIG) {
 			//ある
+			if (SNS_CONFIG.TYPE === "MISSKEY") {
+				const RES = await fetch("https://" + SNS_CONFIG.DOMAIN + "/api/users/show", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({username: USER_NAME})
+				});
 
-			await E.editReply(SNS_CONFIG.NAME);
+				if (RES.ok) {
+					const RESULT = await RES.json();
+					console.log(RESULT);
+	
+					await E.editReply(RESULT.id);
+				}else{
+					if(RES.status === 404){
+						await E.editReply("指定されたアカウントは見つかりませんでした！");
+					}else{
+						await E.editReply("APIがエラーを吐きました");
+					}
+				}
+			} else {
+				await E.editReply("Misskeyしかまだ対応してない");
+			}
 		} else {
 			//無い
 			await E.editReply("インスタンスが見つかりませんでした");
