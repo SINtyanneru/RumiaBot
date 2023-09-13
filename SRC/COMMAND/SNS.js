831d2064bccde17a49925cabdf176d04c9226963
るみさん <irjitgd@gmail.com>
2023-09-13 16:36:05 +0900

    SNS:埋め込み

--- SRC/COMMAND/SNS.js
+++ SRC/COMMAND/SNS.js
@@ -1,4 +1,7 @@
 import { CONFIG } from "../MODULES/CONFIG.js";
+import { RND_COLOR } from "../MODULES/RND_COLOR.js";
+import { MessageEmbed } from "discord.js";
+
 export class SNS {
 	constructor(INTERACTION) {
 		this.E = INTERACTION;
@@ -10,7 +13,7 @@ export class SNS {
 		const USER_NAME = E.options.getString("userid");
 
 		//インスタンスの設定を取得
-		let SNS_CONFIG = CONFIG.SNS.find(ROW => ROW.ID === TYPE);
+		let SNS_CONFIG = CONFIG.SNS.find((ROW) => ROW.ID === TYPE);
 
 		//設定があるか
 		if (SNS_CONFIG) {
@@ -19,20 +22,28 @@ export class SNS {
 				const RES = await fetch("https://" + SNS_CONFIG.DOMAIN + "/api/users/show", {
 					method: "POST",
 					headers: {
-						"Content-Type": "application/json"
+						"Content-Type": "application/json",
 					},
-					body: JSON.stringify({ username: USER_NAME })
+					body: JSON.stringify({username: USER_NAME})
 				});
 
 				if (RES.ok) {
 					const RESULT = await RES.json();
 					console.log(RESULT);
+	
+					//埋め込みつくるマン
+					const EB = new MessageEmbed();
+					EB.setTitle("このチャンネルに「" + RESULT.name + "」さんの投稿を垂れ流します");
+					EB.setDescription("以下の設定で流すよん\n"+
+										"NSFWを隠します"+
+										"画像もテキストもごっちゃまぜで流します");
+					EB.setColor(RND_COLOR());
 
-					await E.editReply(RESULT.id);
-				} else {
-					if (RES.status === 404) {
+					await E.editReply({embeds:[EB]});
+				}else{
+					if(RES.status === 404){
 						await E.editReply("指定されたアカウントは見つかりませんでした！");
-					} else {
+					}else{
 						await E.editReply("APIがエラーを吐きました");
 					}
 				}
