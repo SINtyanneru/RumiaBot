c86970002d857a3a475d9cae79f28f031eade13b
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-09-20 19:04:21 +0900

    冗長な構文を直す的な MapからObjectに戻した

--- SRC/MODULES/LOCK_NICK_NAME.js
+++ SRC/MODULES/LOCK_NICK_NAME.js
@@ -1,19 +1,18 @@
-import { kazemidori, minto, rumiserver, __four__lkmy, midoriReimuChan, massango } from "./SYNTAX_SUGER.js";
+import { kazemidori, minto, rumiserver, __four__lkmy, midoriReimuChan, massango, rumisub } from "./SYNTAX_SUGER.js";
 
 export async function LOCK_NICK_NAME(MEMBER) {
 	try {
 		//るみ鯖無いでの出来事に適応
 		if (MEMBER.guild.id === rumiserver) {
-			const NICK_LOCK_USER = new Map();
-			NICK_LOCK_USER.set(kazemidori, { NAME: "BaGuAr二世" });
-			NICK_LOCK_USER.set(minto, { NAME: "ミント㌨Да！！" });
-			NICK_LOCK_USER.set(__four__lkmy, { NAME: "BaGuAr二世" });
-			NICK_LOCK_USER.set(midoriReimuChan, { NAME: "緑霊夢" });
-			NICK_LOCK_USER.set(massango, { NAME: 'まっさんご"う"' });
-			NICK_LOCK_USER.set(kazemidori, { NAME: "風緑" });
-			NICK_LOCK_USER.set("945939067961499698", { NAME: "Rumi hat alonaaaaaaaaaa" });
-
-			const NLU = NICK_LOCK_USER.get[MEMBER.id.toString()];
+			const NICK_LOCK_USER = {
+				[kazemidori]: "ねこかわ",
+				[minto]: "ミント㌨Да！！",
+				[__four__lkmy]: "BaGuAr二世",
+				[midoriReimuChan]: "ベジタリアン霊夢",
+				[massango]: 'まっさんこ"う"',
+				[rumisub]: "Rumi hat alonaaaaaaaaaa"
+			};
+			const NLU = NICK_LOCK_USER[MEMBER.id.toString()];
 			if (NLU) {
 				if (NLU.NAME !== MEMBER.nickname) {
 					console.log("[ INFO ][ LOCK NICKNAME ]" + MEMBER.user.name + "がニックネームを変えました");
