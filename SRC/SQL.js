260618e75058c5945fc019ed483bbf8bf6918013
るみさん <irjitgd@gmail.com>
2023-09-20 16:13:09 +0900

    は？

--- SRC/SQL.js
+++ SRC/SQL.js
@@ -5,10 +5,10 @@
 import { CONFIG } from "./MODULES/CONFIG.js";
 import mysql from "mysql";
 
-export class SQL{
+export class SQL {
 	constructor() {
 		//接続する設定に成っているか
-		if(CONFIG.SQL_CONNECT){
+		if (CONFIG.SQL_CONNECT) {
 			this.SQL_CONNECTION = mysql.createConnection({
 				host: CONFIG.SQL_HOST,
 				user: CONFIG.SQL_USER,
@@ -16,16 +16,16 @@ export class SQL{
 				database: CONFIG.SQL_DB
 			});
 
-			this.main()
+			this.main();
 		}
 	}
 
-	main(){
-		this.SQL_CONNECTION.connect((ERR) => {
-			if(ERR){
+	main() {
+		this.SQL_CONNECTION.connect(ERR => {
+			if (ERR) {
 				console.log("[ ERR ][ SQL ]" + ERR);
 				return;
-			}else{
+			} else {
 				console.log("[ OK ][ SQL ] Connected!");
 			}
 		});
