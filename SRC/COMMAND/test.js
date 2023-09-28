bb96b6f3f4df3251f6f3682aa100bcf422e05cd7
るみさん <irjitgd@gmail.com>
2023-09-28 18:38:37 +0900

    VSCが勝手にコードを変えてくるので途中で断念

--- SRC/COMMAND/test.js
+++ SRC/COMMAND/test.js
@@ -1,5 +1,10 @@
+/*
 import { MessageEmbed } from "discord.js";
 import { RND_COLOR } from "../MODULES/RND_COLOR.js";
+*/
+
+import { SQL_OBJ } from "../Main.js";
+
 export class test {
 	constructor(INTERACTION) {
 		this.E = INTERACTION;
@@ -7,10 +12,15 @@ export class test {
 
 	async main() {
 		let E = this.E;
-		const embed = new MessageEmbed()
-			.setTitle("テスト")
-			.setDescription("これが見れてるってことは、成功したってことです()")
-			.setColor(RND_COLOR());
-		await E.editReply({ embeds: [embed] });
+
+		let TEXT = "テストを実行します\n";
+
+		await E.editReply(TEXT);
+
+		SQL_OBJ.SCRIPT_RUN("SHOW TABLES LIKE 'USER';", []).then(async RESULT => {
+			console.log(RESULT);
+			TEXT += "[OK]SQL TABLE USER\n";
+			await E.editReply(TEXT);
+		});
 	}
 }
