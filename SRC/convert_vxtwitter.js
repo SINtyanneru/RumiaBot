37c196d11d353fa01d75950de2eee3bdfcd06bbf
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-10-02 22:28:58 +0900

    ホットリロードするやつと、vxtwitterに機能付け足し

--- SRC/convert_vxtwitter.js
+++ SRC/convert_vxtwitter.js
@@ -2,10 +2,11 @@ import { WebHook_FIND } from "./MODULES/WebHook_FIND.js";
 import { sanitize } from "./MODULES/sanitize.js";
 
 export async function convert_vxtwitter(message) {
-	const VX_REGEX = /https:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/[0-9]+/g;
-	if (message.content.match(VX_REGEX)) {
+	const VX_REGEX = /https:\/\/twitter\.com\/([a-zA-Z0-9_]+\/status\/[0-9]+)(\?s=[0-9]*)?/g;
+	const result = message.content.match(VX_REGEX).at(0);
+	if (result) {
 		let WEB_HOOK = await WebHook_FIND(message.channel);
-		const TEXT = sanitize(message.content).replaceAll("https://twitter.com/", "https://vxtwitter.com/");
+		const TEXT = sanitize(message.content).replace(VX_REGEX, "https://vxtwitter.com/$1 [mobile](https://twitter.com/$1)");
 
 		//WHでめっせーじを送る
 		WEB_HOOK.send({
