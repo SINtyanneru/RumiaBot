858fb6e5ea01830c2a79fd3ff81113a2af7ffd44
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-10-22 00:02:31 +0900

    何をしたのかは覚えていないけど、多分Lintした

--- SRC/COMMAND/WH_CLEAR.js
+++ SRC/COMMAND/WH_CLEAR.js
@@ -1,37 +1,37 @@
 import { sanitize } from "../MODULES/sanitize.js";
 
-export class WH_CLEAR{
+export class WH_CLEAR {
 	constructor(INTERACTION) {
 		this.E = INTERACTION;
 	}
 
-	async main(){
-		const PERM = this.E.channel.permissionsFor(this.E.member).has('MANAGE_MESSAGES');
-		if(PERM){
+	async main() {
+		const PERM = this.E.channel.permissionsFor(this.E.member).has("MANAGE_MESSAGES");
+		if (PERM) {
 			let FWH = await this.E.channel.fetchWebhooks();
 
 			FWH = Array.from(FWH);
 
-			if(FWH.length > 0){
-				let TEXT = ""
-	
+			if (FWH.length > 0) {
+				let TEXT = "";
+
 				await this.E.editReply("このチャンネルのWebHookを削除しています。。。\n```" + TEXT.toString() + "");
-		
+
 				for (let I = 0; I < FWH.length; I++) {
 					const WH = FWH[I][1];
-					try{
+					try {
 						WH.delete();
 						TEXT += "[ OK ]" + sanitize(WH.name) + "(" + WH.id + ")" + "\n";
 						this.E.editReply("このチャンネルのWebHookを削除しています。。。\n```" + TEXT.toString() + "```");
-					}catch(EX){
+					} catch (EX) {
 						TEXT += "[ ERR ]" + sanitize(WH.name) + "(" + WH.id + ")" + "\n→" + EX + "\n";
 						this.E.editReply("このチャンネルのWebHookを削除しています。。。\n```" + TEXT.toString() + "```");
 					}
 				}
-			}else{
+			} else {
 				this.E.editReply("このチャンネルにはWebHookがないです");
 			}
-		}else{
+		} else {
 			this.E.editReply("お前には権限がありません、出直せ\nMANAGE_MESSAGES");
 		}
 	}
