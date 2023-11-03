3f6e53ebd529f384254aecdb9163e02967caba82
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-11-03 21:02:28 +0900

    多分いらないやつを解除

--- SRC/MODULES/LOCK_NICK_NAME.js
+++ SRC/MODULES/LOCK_NICK_NAME.js
@@ -1,19 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import { GuildMember } from "discord.js";
-import { kazemidori, minto, rumiserver, __four__lkmy, midoriReimuChan, massango, rumisub, p_nsk, hassy1216, rusklabo, azusa } from "./SYNTAX_SUGER.js";
+import { rumiserver, massango, rumisub, hassy1216, rusklabo, azusa } from "./SYNTAX_SUGER.js";
 /** @param {GuildMember} MEMBER */
 export async function LOCK_NICK_NAME(MEMBER) {
 	try {
 		//るみ鯖無いでの出来事に適応
 		if (MEMBER.guild.id === rumiserver) {
 			const NICK_LOCK_USER = {
-				[kazemidori]: "もふもふ風緑",
-				[minto]: "ミント㌨Да！！",
-				[__four__lkmy]: "BaGuAr二世",
-				[midoriReimuChan]: "ベジタリアン霊夢",
 				[massango]: 'もふもふまっさんこ"う"',
 				[rumisub]: "Rumi hat alonaaaaaaaaaa",
-				[p_nsk]: "プヌスク㌨",
 				[hassy1216]: " もふもふhassyTK",
 				[rusklabo]: " もふもふラスクラボ",
 				[azusa]: "†行動の代償† もふもふ梓"
