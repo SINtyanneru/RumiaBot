150fe63f954662daf1aadfe1024a8e2c855d54dd
SINtyanneru <irjitgd@gmail.com>
2023-11-04 18:47:33 +0900

    vx変換をx.comに対応

--- SRC/convert_vxtwitter.js
+++ SRC/convert_vxtwitter.js
@@ -2,7 +2,7 @@ import { WebHook_FIND } from "./MODULES/WebHook_FIND.js";
 import { sanitize } from "./MODULES/sanitize.js";
 
 export async function convert_vxtwitter(message) {
-	const VX_REGEX = /https:\/\/twitter\.com\/([a-zA-Z0-9_]+\/status\/[0-9]+)(\?s=[0-9]*)?/g;
+	const VX_REGEX = /https:\/\/(?:twitter|x)\.com\/([a-zA-Z0-9_]+\/status\/[0-9]+)(\?s=[0-9]*)?/g;
 	let result = message.content.match(VX_REGEX);
 	if (result) {
 		result = result[0];
