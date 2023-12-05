6b56eae1f12c71afc006314079efba75af1e49dd
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-12-05 21:34:46 +0900

    eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	"env": {
+		"es2021": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [".eslintrc.{js,cjs}"],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": ["error", "tab", { "SwitchCase": 1 }],
+		"linebreak-style": ["error", "unix"],
+		"quotes": ["error", "double", { avoidEscape: true }],
+		"semi": ["error", "always"]
+	}
+};
