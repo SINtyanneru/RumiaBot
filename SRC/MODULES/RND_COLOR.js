3ebc4da1b2355b56945f60057c70cf4f49200ce1
SINtyanneru <irjitgd@gmail.com>
2023-12-05 21:42:07 +0900

    色を乱数生成

--- SRC/MODULES/RND_COLOR.js
+++ SRC/MODULES/RND_COLOR.js
@@ -3,5 +3,8 @@
 
  */
 export function RND_COLOR() {
-	return "#00ff00";
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+	//生成された色が6桁未満なら足りない桁を0で埋める
+	return "#" + "0".repeat(6 - randomColor.length) + randomColor;
 }
