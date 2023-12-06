3269d7064a708a655b35bc16ba242f5c2c09050e
SINtyanneru <irjitgd@gmail.com>
2023-12-07 07:58:35 +0900

    メンバー数取得を別ファイルにしたぜ

--- SRC/Main.js
+++ SRC/Main.js
@@ -37,6 +37,7 @@ import { WS_SERVER } from "./HTTP/WS_SERVER.js";
 import { getMcInfo, getServerInfo, getUserInfo } from "./COMMAND/INFO.js";
 import { REGIST_SLASH_COMMAND } from "./new_REGIST_SL_COMMAND.js";
 import { GET_ALL_MEMBERS_COUNT } from "./MODULES/GET_ALL_GUILD_MEMBERS_COUNT.js";
+import { GET_ALL_MEMBERS_COUNT } from "./MODULES/GET_ALL_GUILD_MEMBERS_COUNT.js";
 
 //ここに、オブジェクトとして置いておくべき、クラスを、置くよ。
 // ↑インスタンスのことですか？←るみさん用語でオブジェクトです
@@ -114,7 +115,7 @@ client.once("ready", async () => {
 		if (TEMP_ACTIVE !== ACTIVE) {
 			TEMP_ACTIVE = ACTIVE;
 			if (ACTIVE) {
-				//活動期間である
+				//活動期間である				//活動期間である
 				client.user.setPresence({
 					status: "online",
 					activities: [
