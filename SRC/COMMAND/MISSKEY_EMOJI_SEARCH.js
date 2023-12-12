7607861a7e6fcdc629e8e14337b0feedb9aba6ef
SINtyanneru <irjitgd@gmail.com>
2023-12-12 22:41:03 +0900

    みすきーのあれ

--- SRC/COMMAND/MISSKEY_EMOJI_SEARCH.js
+++ SRC/COMMAND/MISSKEY_EMOJI_SEARCH.js
@@ -1,11 +1,29 @@
+/**
+ * Misskey絵文字検索
+ */
 export class MISSKEY_EMOJI_SEARCH{
 	constructor(INTERACTION) {
 		this.E = INTERACTION;
+
+		this.CACHE = {};
 	}
 
 	async main() {
 		const EMOJI_NAME = this.E.options.getString("name");
 
-		await this.E.editReply(EMOJI_NAME);
+		const RES = await fetch("https://ussr.rumiserver.com/api/emojis", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+
+		if (RES.ok) {
+			const RESULT = await RES.json();
+			console.log(RESULT);
+		}else{
+			await this.E.editReply("MisskeyのAPIがエラーを吐きやがった！Бля！");
+		}
+
 	}
 }
