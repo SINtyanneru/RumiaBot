8f38cf7c1e40f948a540c4bf4b2f169361d4af19
SINtyanneru <irjitgd@gmail.com>
2023-12-14 23:34:21 +0900

    複数インスタンスにたぶん対応

--- SRC/COMMAND/MISSKEY_EMOJI_SEARCH.js
+++ SRC/COMMAND/MISSKEY_EMOJI_SEARCH.js
@@ -3,6 +3,7 @@
  */
 import { MessageEmbed } from "discord.js";
 import { RND_COLOR } from "../MODULES/RND_COLOR.js";
+import { CONFIG } from "../MODULES/CONFIG.js";
 
 let EMOJI_CACHE = undefined;
 
@@ -21,51 +22,67 @@ export class MISSKEY_EMOJI_SEARCH{
 			let EMOJI_SEARCH_RESULT = [];
 
 			//検索する
-			for (let I = 0; I < EMOJI_CACHE["ussr_rumiserver_com"].length; I++) {
-				const EMOJI_DATA = EMOJI_CACHE["ussr_rumiserver_com"][I];
-				if(EMOJI_DATA.name === EMOJI_NAME || EMOJI_DATA.aliases === EMOJI_NAME){
-					EMOJI_SEARCH_RESULT.push(EMOJI_DATA);
+			for (let I = 0; I < CONFIG.SNS.length; I++) {//複数のインスタンスを回る
+				const SNS = CONFIG.SNS[I];
+				if(SNS.TYPE === "MISSKEY"){
+					for (let I = 0; I < EMOJI_CACHE.DATA[SNS.DOMAIN.replace(".", "_")].length; I++) {
+						const EMOJI_DATA = EMOJI_CACHE.DATA[SNS.DOMAIN.replace(".", "_")][I];
+						if(EMOJI_DATA.name === EMOJI_NAME || EMOJI_DATA.aliases === EMOJI_NAME){
+							EMOJI_SEARCH_RESULT.push(EMOJI_DATA);
+						}
+					}
 				}
 			}
 
+			//検索結果を埋め込み化
 			if(EMOJI_SEARCH_RESULT.length !== 0){
 				let EMOJI_SEARCH_RESULT_EB = [];
 
+				//検索結果は複数有る場合が有るのでfor
 				for (let I = 0; I < EMOJI_SEARCH_RESULT.length; I++) {
 					const ROW = EMOJI_SEARCH_RESULT[I];
 
 					const EB = new MessageEmbed();
 					EB.setColor(RND_COLOR());
 					EB.setTitle(":" + ROW.name + ":");
-					EB.setDescription("るみすきー");
+					EB.setDescription("検索結果");
 					EB.setImage(ROW.url);
 
 					EMOJI_SEARCH_RESULT_EB.push(EB);
 				}
 
+				//返す
 				await this.E.editReply({ embeds: EMOJI_SEARCH_RESULT_EB });
 			}else{
 				await this.E.editReply("そんな絵文字は無い！");
 			}
 		}catch(EX){
+			console.log(EX);
 			await this.E.editReply("DiscordAPIがエラーを吐きやがった！Бля！");
 		}
 	}
 
-	async API_RUN(DOMAIN){
-		const RES = await fetch("https://" + DOMAIN + "/api/emojis", {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json"
-			}
-		});
+	async API_RUN(){
+		let API_RESULT = {};
 
-		if (RES.ok) {
-			const RESULT = await RES.json();
-			return RESULT;
-		}else{
-			return null;
+		for (let I = 0; I < CONFIG.SNS.length; I++) {
+			const SNS = CONFIG.SNS[I];
+			if(SNS.TYPE === "MISSKEY"){
+				const RES = await fetch("https://" + SNS.DOMAIN + "/api/emojis", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				});
+		
+				if (RES.ok) {
+					const RESULT = await RES.json();
+					API_RESULT[SNS.DOMAIN.replace(".", "_")] = RESULT.emojis;
+				}
+			}
 		}
+
+		return API_RESULT;
 	}
 
 	async CACHE(){
@@ -74,11 +91,7 @@ export class MISSKEY_EMOJI_SEARCH{
 			EMOJI_CACHE = {};
 			EMOJI_CACHE["DATE"] = new Date();
 
-			let RESULT = await this.API_RUN("ussr.rumiserver.com");
-			if(RESULT){//取得成功
-				EMOJI_CACHE["ussr_rumiserver_com"] = RESULT.emojis;
-			}
-
+			EMOJI_CACHE["DATA"] = await this.API_RUN();
 			//返す
 			return EMOJI_CACHE;
 		}else{
@@ -86,10 +99,7 @@ export class MISSKEY_EMOJI_SEARCH{
 			if(EMOJI_CACHE["DATE"] - new Date() >= 5 * 60 * 1000){
 				EMOJI_CACHE["DATE"] = new Date();
 
-				let RESULT = await this.API_RUN("ussr.rumiserver.com");
-				if(RESULT){//取得成功
-					EMOJI_CACHE["ussr_rumiserver_com"] = RESULT.emojis;
-				}
+				EMOJI_CACHE["DATA"] = await this.API_RUN();
 
 				//返す
 				return EMOJI_CACHE;
