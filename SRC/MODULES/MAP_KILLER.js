0310517ea540ac5aada497f185602ef21a0933cc
SINtyanneru <irjitgd@gmail.com>
2023-12-29 16:33:36 +0900

    Mapに敵意むき出しなスクリプトを書いた

--- SRC/MODULES/MAP_KILLER.js
+++ SRC/MODULES/MAP_KILLER.js
@@ -5,5 +5,15 @@
 
 //Mapだけは許されないので、MapをArrayにする
 export function MAP_KILLER(MAP){
-	
+	let RESULT_ARRAY = {};
+
+	let MAP_KEYS = Array.from(MAP.keys());
+
+	for (let I = 0; I < MAP_KEYS.length; I++) {
+		const KEY = MAP_KEYS[I];
+		const VALUE = MAP.get(KEY);
+		RESULT_ARRAY[KEY] = VALUE;
+	}
+
+	return RESULT_ARRAY;
 }
