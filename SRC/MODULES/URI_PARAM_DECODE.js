a15eb6fab04e130824255eca6f8c03ee8037b5d7
SINtyanneru <irjitgd@gmail.com>
2024-01-06 21:20:26 +0900

    URIパラメーターをデコードするやつ

---
+++ SRC/MODULES/URI_PARAM_DECODE.js
@@ -0,0 +1,16 @@
+/**
+ * URIパラメーターを解析してArrayにするやつ
+ * @param {URIだよFuck} URI 
+ * @returns 
+ */
+
+export function URI_PARAM_DECODE(URI){
+	let URI_PARAM = {};
+
+	for (let I = 0; I < URI.split("?")[1].split("&").length; I++) {
+		const PARAM = URI.split("?")[1].split("&")[I].split("=");
+		URI_PARAM[PARAM[0]] = PARAM[1];
+	}
+
+	return URI_PARAM;
+}
