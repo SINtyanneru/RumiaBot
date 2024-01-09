17b151a3a77fcb10ea8d970a3403b09562c6ef87
SINtyanneru <irjitgd@gmail.com>
2024-01-10 03:43:45 +0900

    変数を大文字に

--- SRC/Main.py
+++ SRC/Main.py
@@ -3,21 +3,21 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 #カスタムハンドラを作成
 class MyHandler(BaseHTTPRequestHandler):
 	#リクエストが来たらこの関数が実行される
-	def do_GET(self):
-		REQUEST_URI = self.path
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
-		self.wfile.write(b"python de HTTP")
+	def do_GET(SELF):
+		REQUEST_URI = SELF.path
+		SELF.send_response(200)
+		SELF.send_header('Content-type', 'text/html')
+		SELF.end_headers()
+		SELF.wfile.write(b"python de HTTP")
 
 #サーバーのアドレスとポートを指定
 HOST = "0.0.0.0"
 PORT = 8085
 
 #サーバーを作成してハンドラを登録
-server = HTTPServer((HOST, PORT), MyHandler)
+SERVER = HTTPServer((HOST, PORT), MyHandler)
 
 print(f"Starting server on http://{HOST}:{PORT}")
 
 #サーバーを開始
-server.serve_forever()
+SERVER.serve_forever()
