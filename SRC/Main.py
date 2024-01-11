e560dbded5e094a8a7c5258a740813159c43588e
SINtyanneru <irjitgd@gmail.com>
2024-01-11 23:11:39 +0900

    整形

--- SRC/Main.py
+++ SRC/Main.py
@@ -1,10 +1,9 @@
 from HTTP.HTTP_SERVER import CREATE_HTTP_SERVER
-import sys
 
 def main():
 	print("RumiaBOT Python Script")
 
-	CREATE_HTTP_SERVER("0.0.0.0", 8085)
+	CREATE_HTTP_SERVER("0.0.0.0", 3000)
 
-
+#main関数を実行
 main()
