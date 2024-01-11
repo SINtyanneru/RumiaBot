d76158dc8db7bf9a2f1d0238b31be3cbf0ba0e62
SINtyanneru <irjitgd@gmail.com>
2024-01-11 22:57:12 +0900

    マルチスレッド化したらPythonの出力がでてきた！！１

--- SRC/HTTP/HTTP_SERVER.py
+++ SRC/HTTP/HTTP_SERVER.py
@@ -1,18 +1,25 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
 import os
+
+CONTENTS_OWNER_PATH = os.getcwd() + "/SRC/HTTP/CONTENTS"
 
 #カスタムハンドラを作成
 class HTTP_Handler(BaseHTTPRequestHandler):
 	#リクエストが来たらこの関数が実行される
 	def do_GET(SELF):
-		REQUEST_URI = SELF.path
-		if(REQUEST_URI == "/"):
-			F = open("./SRC/HTTP/CONTENTS/index.html", "r", encoding="UTF-8")
+		REQUEST_URI = SELF.path.replace("../", "")
+		CONTENTS = FILE_LOAD(REQUEST_URI)
+		if(CONTENTS is not None):
 			SELF.send_response(200)
 			SELF.send_header("Content-type", "text/html; charset=UTF-8")
 			SELF.end_headers()
-			SELF.wfile.write(F.read().encode("utf-8"))
-			F.close()
+			SELF.wfile.write(CONTENTS.encode("UTF-8"))
+		else:
+			SELF.send_response(404)
+			SELF.send_header("Content-type", "text/html; charset=UTF-8")
+			SELF.end_headers()
+			SELF.wfile.write("404 Page not found".encode("UTF-8"))
 
 #HTTP鯖を起動する関数
 def CREATE_HTTP_SERVER(HOST:str, PORT:int):
@@ -23,4 +30,23 @@ def CREATE_HTTP_SERVER(HOST:str, PORT:int):
 	print(f"Start HTTP server\n{HOST}:{PORT}")
 
 	#サーバーを開始
-	SERVER.serve_forever()
+	HTTP_SERVER_TH = Thread(target=SERVER.serve_forever)
+	HTTP_SERVER_TH.start()
+
+def FILE_LOAD(PATH:str):
+	print("HTTP Reqest:" + CONTENTS_OWNER_PATH + PATH)
+	#パスの先はファイルか
+	if(os.path.isfile(CONTENTS_OWNER_PATH + PATH)):
+		F = open(CONTENTS_OWNER_PATH + PATH)
+		CONTENTS = F.read()
+		F.close()
+		return CONTENTS
+	#ファイルじゃない
+	elif(os.path.exists(CONTENTS_OWNER_PATH + "/index.html")):
+		F = open(CONTENTS_OWNER_PATH + PATH + "index.html")
+		CONTENTS = F.read()
+		F.close()
+		return CONTENTS
+	#そもそもファイル自体無い
+	else:
+		return None
--- SRC/Main.py
+++ SRC/Main.py
@@ -1,6 +1,9 @@
 from HTTP.HTTP_SERVER import CREATE_HTTP_SERVER
+import sys
 
 def main():
+	print("RumiaBOT Python Script")
+
 	CREATE_HTTP_SERVER("0.0.0.0", 8085)
 
 
