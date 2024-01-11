177e21a4ba6895bffa5daff228112c2258fabb51
SINtyanneru <irjitgd@gmail.com>
2024-01-11 22:00:32 +0900

    ファイルを読み込ませた

--- SRC/HTTP/HTTP_SERVER.py
+++ SRC/HTTP/HTTP_SERVER.py
@@ -5,10 +5,13 @@ class HTTP_Handler(BaseHTTPRequestHandler):
 	#リクエストが来たらこの関数が実行される
 	def do_GET(SELF):
 		REQUEST_URI = SELF.path
-		SELF.send_response(200)
-		SELF.send_header('Content-type', 'text/html')
-		SELF.end_headers()
-		SELF.wfile.write(b"python de HTTP")
+		if(REQUEST_URI == "/"):
+			F = open("/home/rumisan/source/RumiaBot/SRC/HTTP/CONTENTS/index.html", "r", encoding="UTF-8")
+			SELF.send_response(200)
+			SELF.send_header("Content-type", "text/html; charset=UTF-8")
+			SELF.end_headers()
+			SELF.wfile.write(F.read().encode("utf-8"))
+			F.close()
 
 #HTTP鯖を起動する関数
 def CREATE_HTTP_SERVER(HOST:str, PORT:int):
