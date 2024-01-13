0afcb467913d4dd83e7ff0cb152263f2e517c138
SINtyanneru <irjitgd@gmail.com>
2024-01-13 19:18:46 +0900

    メソッドの設定を追加

--- SRC/MODULES_PYTHON/AJAX.py
+++ SRC/MODULES_PYTHON/AJAX.py
@@ -6,13 +6,16 @@ def AJAX(URL:str, SETTING:dict):
 	try:
 		#ヘッダー
 		HEADER = {}
+		METHOD = "GET"
 
 		#設定を読み込むm
 		if(SETTING["HEADER"] is not None):
 			HEADER = SETTING["HEADER"]
+		if(SETTING["METHOD"] is not None):
+			METHOD = SETTING["METHOD"]
 
 		#リクエストを送る
-		REQ = request.Request(URL, headers=HEADER)
+		REQ = request.Request(URL, headers=HEADER, method=METHOD)
 		with request.urlopen(REQ) as RES:
 			RESULT = RES.read()
 		return RESULT
