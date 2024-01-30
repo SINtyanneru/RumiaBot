9070e15b80bd41e8e7b13ca8a0cbea6dd78b5569
SINtyanneru <irjitgd@gmail.com>
2024-01-30 19:11:28 +0900

    設定

--- SRC/COMMAND/SETTING.js
+++ SRC/COMMAND/SETTING.js
@@ -14,7 +14,21 @@ export class SETTING {
 		.setDescription("設定します")
 		.addStringOption(o =>
 			o
-				.setName("mode")
+				.setName("function")
+				.setDescription("どの機能を？")
+				.setChoices({
+					name: "VXTwitterに置き換え機能",
+					value: "vxtwitter"
+				},
+				{
+					name: "あいさつ機能",
+					value: "guten_morgen"
+				})
+				.setRequired(true)
+		)
+		.addStringOption(o =>
+			o
+				.setName("enable")
 				.setDescription("設定をどうするか")
 				.setChoices(
 					{
@@ -30,76 +44,132 @@ export class SETTING {
 		)
 		.addStringOption(o =>
 			o
-				.setName("function")
-				.setDescription("どの機能を？")
-				.setChoices({
-					name: "VXTwitterに置き換え機能",
-					value: "vxtwitter"
-				})
+				.setName("mode")
+				.setDescription("モード")
+				.setChoices(
+					{
+						name: "鯖全体の設定",
+						value: "GUILD"
+					},
+					{
+						name: "チャンネル",
+						value: "CHANNEL"
+					}
+				)
 				.setRequired(true)
 		);
 	constructor(INTERACTION) {
 		this.E = INTERACTION;
+
+		this.FUNCTION_LIST = [
+			{
+				ID:"vxtwitter",
+				GUILD:true,
+				CHANNEL:false
+			},
+			{
+				ID:"guten_morgen",
+				GUILD:false,
+				CHANNEL:true
+			}
+		];
 	}
 	async SET() {
 		if (CONFIG.SQL.SQL_CONNECT) {
 			const FUNCTION = this.E.options.getString("function");
+			const ENABLE = this.E.options.getString("enable");
 			const MODE = this.E.options.getString("mode");
-			if (MODE === "true") {
-				//有効化する
-				try {
-					const RESULT = await SQL_OBJ.SCRIPT_RUN(
-						"SELECT * FROM `CONFIG` WHERE `GID` = ? AND `FUNC_ID` = ?",
-						[this.E.guild.id, FUNCTION]
-					);
+			const FUNCTION_INFO = this.FUNCTION_LIST.find((ROW) => FUNCTION === ROW.ID);
+			if(FUNCTION_INFO){
+				if(MODE === "GUILD"){
+					if(!FUNCTION_INFO.GUILD){
+						await this.E.editReply("鯖全体への適応に未対応の機能です");
+						return;
+					}
+				}
+				if(MODE === "CHANNEL"){
+					if(!FUNCTION_INFO.CHANNEL){
+						await this.E.editReply("チャンネルへの適応に未対応の機能です");
+						return;
+					}
+				}
 
-					if (RESULT.length === 0) {
-						try {
-							await SQL_OBJ.SCRIPT_RUN(
-								"INSERT INTO `CONFIG` (`ID`, `GID`, `MODE`, `FUNC_ID`) VALUES (NULL, ?, '1', ?);",
+				//設定処理
+				if (ENABLE === "true") {
+					//有効化する
+					try {
+						const RESULT = await SQL_OBJ.SCRIPT_RUN(
+							"SELECT * FROM `CONFIG` WHERE `GID` = ? AND `FUNC_ID` = ?",
+							[this.E.guild.id, FUNCTION]
+						);
+	
+						if (RESULT.length === 0) {
+							try {
+								if(MODE === "GUILD"){
+									await SQL_OBJ.SCRIPT_RUN(
+										"INSERT INTO `CONFIG` (`ID`, `GID`, `CID`, `MODE`, `FUNC_ID`) VALUES (NULL, ?, '', '1', ?);",
+										[this.E.guild.id, FUNCTION]
+									);
+								}else{
+									await SQL_OBJ.SCRIPT_RUN(
+										"INSERT INTO `CONFIG` (`ID`, `GID`, `CID`, `MODE`, `FUNC_ID`) VALUES (NULL, ?, ?, '2', ?);",
+										[this.E.guild.id, this.E.channel.id, FUNCTION]
+									);
+								}
+	
+								await this.E.editReply(FUNCTION + "を有効化したとおもいます、たぶん");
+								//再読込
+								FUNCTION_SETTING_OBJ.LOAD();
+							} catch (EX) {
+								console.error("[ ERR in Promise ][ SETTING ]", EX);
+								await this.E.editReply("エラー\n" + EX);
+							}
+						} else {
+							await this.E.editReply("すでに設定済み");
+						}
+					} catch (EX) {
+						console.error("[ ERR in Promise ][ SETTING ]", EX);
+						await this.E.editReply("エラー\n" + EX);
+					}
+				} else {
+					//無効化する
+					try {
+						let RESULT;
+						if(FUNCTION_INFO.GUILD){
+							RESULT = await SQL_OBJ.SCRIPT_RUN(
+								"SELECT * FROM `CONFIG` WHERE `GID` = ? AND `FUNC_ID` = ?",
 								[this.E.guild.id, FUNCTION]
 							);
-
-							await this.E.editReply(FUNCTION + "を有効化したとおもいます、たぶん");
-							//再読込
-							FUNCTION_SETTING_OBJ.LOAD();
-						} catch (EX) {
-							console.error("[ ERR in Promise ][ SETTING ]", EX);
-							await this.E.editReply("エラー\n" + EX);
 						}
-					} else {
-						await this.E.editReply("すでに設定済み");
-					}
-				} catch (EX) {
-					console.error("[ ERR in Promise ][ SETTING ]", EX);
-					await this.E.editReply("エラー\n" + EX);
-				}
-			} else {
-				//無効化する
-				try {
-					const RESULT = await SQL_OBJ.SCRIPT_RUN(
-						"SELECT * FROM `CONFIG` WHERE `GID` = ? AND `FUNC_ID` = ?",
-						[this.E.guild.id, FUNCTION]
-					);
 
-					if (RESULT.length !== 0) {
-						try {
-							await SQL_OBJ.SCRIPT_RUN("DELETE FROM `CONFIG` WHERE `ID` = ?", [RESULT[0].ID]);
-
-							await this.E.editReply(FUNCTION + "を無効化したとおもいます、たぶん");
-							//再読込
-							FUNCTION_SETTING_OBJ.LOAD();
-						} catch (EX) {
-							console.error("[ ERR in Promise ][ SETTING ]", EX);
-							await this.E.editReply("エラー\n" + EX);
+						if(FUNCTION_INFO.CHANNEL){
+							RESULT = await SQL_OBJ.SCRIPT_RUN(
+								"SELECT * FROM `CONFIG` WHERE `GID` = ? AND `CID` = ? AND `FUNC_ID` = ?",
+								[this.E.guild.id, this.E.channel.id, FUNCTION]
+							);
 						}
-					} else {
-						await this.E.editReply("すでに無効化済み");
+	
+						if (RESULT.length !== 0) {
+							try {
+								await SQL_OBJ.SCRIPT_RUN("DELETE FROM `CONFIG` WHERE `ID` = ?", [RESULT[0].ID]);
+	
+								await this.E.editReply(FUNCTION + "を無効化したとおもいます、たぶん");
+								//再読込
+								FUNCTION_SETTING_OBJ.LOAD();
+							} catch (EX) {
+								console.error("[ ERR in Promise ][ SETTING ]", EX);
+								await this.E.editReply("エラー\n" + EX);
+							}
+						} else {
+							await this.E.editReply("すでに無効化済み");
+						}
+					} catch (EX) {
+						console.error("[ ERR in Promise ][ SETTING ]", EX);
+						await this.E.editReply("エラー\n" + EX);
 					}
-				} catch (EX) {
-					console.error("[ ERR in Promise ][ SETTING ]", EX);
-					await this.E.editReply("エラー\n" + EX);
 				}
+			}else{
+				await this.E.editReply("?");
 			}
 		} else {
 			await this.E.editReply("SQLに接続されていません");
