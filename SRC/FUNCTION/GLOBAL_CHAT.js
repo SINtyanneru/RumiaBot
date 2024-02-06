b41d01caeb4641704953954237e79e3401f0c524
SINtyanneru <irjitgd@gmail.com>
2024-02-06 16:04:29 +0900

    権限問題とか

--- SRC/FUNCTION/GLOBAL_CHAT.js
+++ SRC/FUNCTION/GLOBAL_CHAT.js
@@ -32,24 +32,26 @@ export class GLOBAL_CHAT{
 		try{
 			const G_CHANNEL = this.CHANNEL_LIST.find((ROW) => ROW.CID === message.channel.id);
 
-			let FWH = await message.channel.fetchWebhooks();
-			let WH = FWH.find(webhook => webhook.id === message.author.id);
+			if(G_CHANNEL){
+				let FWH = await message.channel.fetchWebhooks();
+				let WH = FWH.find(webhook => webhook.id === message.author.id);
+
+				if(!WH){
+					console.log("ルーム" + G_CHANNEL.ROOM + "に送信したよ");
 	
-			if(G_CHANNEL && !WH){
-				console.log("ルーム" + G_CHANNEL.ROOM + "に送信したよ");
-	
-				for (let I = 0; I < this.CHANNEL_LIST.length; I++) {
-					const CHANNEL_DATA = this.CHANNEL_LIST[I];
-					if(CHANNEL_DATA.ROOM === G_CHANNEL.ROOM && CHANNEL_DATA.CID !== message.channel.id){
-						const CHANNEL = await client.channels.fetch(CHANNEL_DATA.CID);
-						if(CHANNEL){
-							let WEB_HOOK = await WebHook_FIND(CHANNEL);
-		
-							WEB_HOOK.send({
-								username: message.author.username + "@" + message.guild.name,
-								avatarURL: "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png",
-								content: sanitize(message.content)
-							});
+					for (let I = 0; I < this.CHANNEL_LIST.length; I++) {
+						const CHANNEL_DATA = this.CHANNEL_LIST[I];
+						if(CHANNEL_DATA.ROOM === G_CHANNEL.ROOM && CHANNEL_DATA.CID !== message.channel.id){
+							const CHANNEL = await client.channels.fetch(CHANNEL_DATA.CID);
+							if(CHANNEL){
+								let WEB_HOOK = await WebHook_FIND(CHANNEL);
+			
+								WEB_HOOK.send({
+									username: message.author.username + "@" + message.guild.name,
+									avatarURL: "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png",
+									content: sanitize(message.content)
+								});
+							}
 						}
 					}
 				}
