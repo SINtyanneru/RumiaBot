581ef83ca78e29a911eb6b8413d932f1c4d13a75
SINtyanneru <irjitgd@gmail.com>
2024-02-22 19:34:23 +0900

    TELNET

--- JAVA/src/main/java/com/rumisystem/rumiabot/Main.java
+++ JAVA/src/main/java/com/rumisystem/rumiabot/Main.java
@@ -23,7 +23,7 @@ public class Main {
 		WS.start();
 
 		Thread PS1 = new Thread( new ProcessStart("JS", new String[]{"node", "./SRC/JS/Main.js"}));
-		//PS1.start();
+		PS1.start();
 
 		//Thread PS2 =new Thread( new ProcessStart("PY", "python", "./SRC/PY/Main.py"));
 		//PS2.start();
