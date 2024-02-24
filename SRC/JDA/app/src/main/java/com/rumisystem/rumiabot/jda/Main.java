3cace7193d5f93e37a721f3989bcf312354c709d
SINtyanneru <irjitgd@gmail.com>
2024-02-24 22:43:07 +0900

    これを設定しないとメンバー一覧がおかしくなるっぽい

--- SRC/JDA/app/src/main/java/com/rumisystem/rumiabot/jda/Main.java
+++ SRC/JDA/app/src/main/java/com/rumisystem/rumiabot/jda/Main.java
@@ -11,6 +11,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Main {
 	public static JDA BOT = null;
@@ -27,22 +28,31 @@ public class Main {
 				//JDAビルダーを作る
 				JDABuilder JDA_BUILDER = JDABuilder.createDefault(
 						CONFIG.CONFIG_DATA.get("DISCORD").get("TOKEN").asText(),
-						GatewayIntent.MESSAGE_CONTENT,
 						GatewayIntent.GUILD_MEMBERS,
-						GatewayIntent.GUILD_MESSAGES,
-						GatewayIntent.GUILD_PRESENCES,
-						GatewayIntent.GUILD_VOICE_STATES,
+						GatewayIntent.GUILD_BANS,
+						GatewayIntent.GUILD_MODERATION,
 						GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
-						GatewayIntent.SCHEDULED_EVENTS,
+						GatewayIntent.GUILD_WEBHOOKS,
+						GatewayIntent.GUILD_INVITES,
+						GatewayIntent.GUILD_VOICE_STATES,
+						GatewayIntent.GUILD_PRESENCES,
+						GatewayIntent.GUILD_MESSAGES,
+						GatewayIntent.GUILD_MESSAGE_REACTIONS,
+						GatewayIntent.GUILD_MESSAGE_TYPING,
 						GatewayIntent.DIRECT_MESSAGES,
 						GatewayIntent.DIRECT_MESSAGE_REACTIONS,
-						GatewayIntent.DIRECT_MESSAGE_TYPING
+						GatewayIntent.DIRECT_MESSAGE_TYPING,
+						GatewayIntent.MESSAGE_CONTENT,
+						GatewayIntent.SCHEDULED_EVENTS,
+						GatewayIntent.AUTO_MODERATION_CONFIGURATION,
+						GatewayIntent.AUTO_MODERATION_EXECUTION
 				);
 
 				//設定
 				JDA_BUILDER.setRawEventsEnabled(true);
 				JDA_BUILDER.setEventPassthrough(true);
 				JDA_BUILDER.addEventListeners(new DiscordEvent());
+				JDA_BUILDER.setMemberCachePolicy(MemberCachePolicy.ALL);
 
 				//ステータス
 				JDA_BUILDER.setActivity(Activity.watching("貴様"));
@@ -64,9 +74,9 @@ public class Main {
 	}
 
 	public static void REGIST_SLASHCOMMAND(){
-		SlashCommandData test = Commands.slash("test", "テスト用").setNameLocalization(DiscordLocale.JAPANESE, "テスト");
-		SlashCommandData info_server = Commands.slash("info_server", "鯖の情報を取得").setNameLocalization(DiscordLocale.JAPANESE, "サーバーの情報");
-		SlashCommandData info_user = Commands.slash("info_user", "ユーザー情報取得").setNameLocalization(DiscordLocale.JAPANESE, "ユーザー情報")
+		SlashCommandData test = Commands.slash("test", "テスト用");
+		SlashCommandData info_server = Commands.slash("info_server", "鯖の情報を取得");
+		SlashCommandData info_user = Commands.slash("info_user", "ユーザー情報取得")
 				.addOption(OptionType.USER, "user", "ユーザーを指定しろ", false);
 
 		BOT.updateCommands().addCommands(
