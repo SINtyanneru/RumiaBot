433b252b1e46c6e1944a4e69296d6766b213ce37
SINtyanneru <irjitgd@gmail.com>
2024-02-27 21:26:55 +0900

    Pixivから画像を落とすための関数

--- SRC/JDA/app/src/main/java/com/rumisystem/rumiabot/jda/MODULE/HTTP_REQUEST.java
+++ SRC/JDA/app/src/main/java/com/rumisystem/rumiabot/jda/MODULE/HTTP_REQUEST.java
@@ -114,8 +114,35 @@ public class HTTP_REQUEST {
 		}
 	}
 
-	public String PIXIV(){
+	public String PIXIV_DOWNLOAD(String PATH){
+		try{
+			//名前が長すぎるので切り落としたよ
+			HttpURLConnection HUC = (HttpURLConnection) REQIEST_URI.openConnection();
 
+			//GETリクエストだと主張する
+			HUC.setRequestMethod("GET");
+
+			//ヘッダーを入れる
+			HUC.setRequestProperty("Host", "i.pximg.net");
+			HUC.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+			HUC.setRequestProperty("Referer", "https://www.pixiv.net/");
+
+			//レスポンスコード
+			int RES_CODE = HUC.getResponseCode();
+			if(RES_CODE == HttpURLConnection.HTTP_OK){
+				//ファイルを保存する機構
+				InputStream IS = HUC.getInputStream();
+				FileOutputStream OS = new FileOutputStream(PATH);
+				byte[] BUFFER = new byte[4096];
+				int BYTES_READ;
+				while((BYTES_READ = IS.read(BUFFER)) != -1){
+					OS.write(BUFFER, 0, BYTES_READ);
+				}
+
+			}
+		}catch (Exception EX){
+			EX.printStackTrace();
+		}
 	}
 
 	//ダウンロード
@@ -128,9 +155,7 @@ public class HTTP_REQUEST {
 			HUC.setRequestMethod("GET");
 
 			//ヘッダーを入れる
-			HUC.setRequestProperty("Host", "i.pximg.net");
 			HUC.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-			HUC.setRequestProperty("Referer", "https://www.pixiv.net/");
 
 			//レスポンスコード
 			int RES_CODE = HUC.getResponseCode();
