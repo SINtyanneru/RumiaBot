a9a93d27dde71930179972561956ee59ff8ead76
SINtyanneru <irjitgd@gmail.com>
2024-10-07 00:22:59 +0900

    誤字修正

--- src/main/java/com/rumisystem/rumiabot/Discord/FUNCTION/MESSAGE_INFO.java
+++ src/main/java/com/rumisystem/rumiabot/Discord/FUNCTION/MESSAGE_INFO.java
@@ -9,10 +9,10 @@ public class MESSAGE_INFO {
 		String TEXT =REPLY_MSG.getContentRaw();
 		TEXT = TEXT.replaceAll("`", "\\`");
 
-		MSG.reply(	"# Messēżi talōg\n"
+		MSG.reply(	"# Messēżi talå̄g\n"
 				+ "tvais:" + DATE_FORMAT.KOUKI(REPLY_MSG.getTimeCreated()) + "\n"
 				+ "tvais(naṡonal):" + DATE_FORMAT.ZHUUNI_H(REPLY_MSG.getTimeCreated()) + "\n"
-				+ "Sendeljo:" + REPLY_MSG.getAuthor().getName() + "(" + REPLY_MSG.getAuthor().getId() + ")" + "\n"
+				+ "Sendeljå:" + REPLY_MSG.getAuthor().getName() + "(" + REPLY_MSG.getAuthor().getId() + ")" + "\n"
 				+ "Messēżi ID:" + REPLY_MSG.getId() + "\n"
 				+ "```\n" + TEXT + "\n```"
 		).queue();
