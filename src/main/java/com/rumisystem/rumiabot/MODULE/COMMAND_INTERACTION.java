068fbd93eea8b4fb2c7984114b77a2b591301dc2
SINtyanneru <irjitgd@gmail.com>
2024-11-26 06:11:23 +0900

    はい

--- src/main/java/com/rumisystem/rumiabot/MODULE/COMMAND_INTERACTION.java
+++ src/main/java/com/rumisystem/rumiabot/MODULE/COMMAND_INTERACTION.java
@@ -1,5 +1,14 @@
 package com.rumisystem.rumiabot.MODULE;
 
-public class COMMAND_INTERACTION {
+import java.util.HashMap;
+import java.util.List;
 
+public class COMMAND_INTERACTION {
+	private String NAME;
+	private List<HashMap<String, String>> OPTION_LIST;
+
+	public COMMAND_INTERACTION(String NAME, List<HashMap<String, String>> OPTION_LIST) {
+		this.NAME = NAME;
+		this.OPTION_LIST = OPTION_LIST;
+	}
 }
