94fafcd605e5c6900f5a8c8383018db01f225c35
SINtyanneru <irjitgd@gmail.com>
2025-02-13 13:05:10 +0900

    ブラック・リストのやつ

--- System/src/main/java/su/rumishistem/rumiabot/System/Discord/DiscordEventListener.java
+++ System/src/main/java/su/rumishistem/rumiabot/System/Discord/DiscordEventListener.java
@@ -1,11 +1,22 @@
 package su.rumishistem.rumiabot.System.Discord;
 
 import static su.rumishistem.rumiabot.System.Main.FunctionModuleList;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import static su.rumishistem.rumiabot.System.Main.DISCORD_BOT;
+
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import su.rumishistem.rumi_java_lib.ArrayNode;
 import su.rumishistem.rumi_java_lib.EXCEPTION_READER;
+import su.rumishistem.rumi_java_lib.SQL;
 import su.rumishistem.rumiabot.System.MODULE.SearchCommand;
 import su.rumishistem.rumiabot.System.MODULE.UserBlockCheck;
 import su.rumishistem.rumiabot.System.TYPE.CommandData;
@@ -60,6 +71,71 @@ public class DiscordEventListener extends ListenerAdapter {
 		} catch (Exception EX) {
 			String EX_TEXT = EXCEPTION_READER.READ(EX);
 			INTERACTION.getHook().editOriginal("エラー\n```\n" + EX_TEXT + "\n```").queue();
+			EX.printStackTrace();
+		}
+	}
+
+	@Override//鯖に参加
+	public void onGuildJoin(GuildJoinEvent E){
+		try {
+			//通知
+			TextChannel CH = DISCORD_BOT.getTextChannelById("1128742498194444298");
+			if(CH != null){
+				CH.sendMessage( E.getGuild().getName().replace("@", "AD") + "に参加しました！\n" +
+						"これで" + DISCORD_BOT.getGuilds().size() + "個の鯖に参加しました。").queue();
+			}
+
+			//ブラックリスト
+			ArrayNode RESULT = SQL.RUN("SELECT * FROM `GUILD_BLACKLIST` WHERE `GID` = ?", new Object[] {E.getGuild().getId()});
+			if (RESULT.get(0) != null) {
+				ArrayNode INFO = RESULT.get(0);
+				if (E.getGuild().getOwner() != null) {
+					//オーナーのDMを開く
+					E.getGuild().getOwner().getUser().openPrivateChannel().queue((DM)->{
+						try {
+							StringBuilder TEXT = new StringBuilder();
+							TEXT.append("あなたのサーバーはブラックリストに入っています\n");
+							TEXT.append("理由：" + INFO.getData("RESON").asString() + "\n");
+
+							//DM送信
+							DM.sendMessage(TEXT.toString()).queue();
+						} catch (Exception EX) {
+							EX.printStackTrace();
+						} finally {
+							//脱退
+							E.getGuild().leave().queue();
+						}
+					});
+				} else {
+					//脱退
+					E.getGuild().leave().queue();
+				}
+
+				if(CH != null){
+					CH.sendMessage("残念ながらブラックリスト入りしていたサーバーでした、脱退します").queue();
+				}
+			}
+		} catch (Exception EX) {
+			EX.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onGuildLeave(GuildLeaveEvent E){
+		try {
+			//ブラックリスト
+			SQL.UP_RUN("INSERT INTO `GUILD_BLACKLIST` (`GID`, `RESON`) VALUES (?, ?)", new Object[] {
+				E.getGuild().getId(),
+				LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日E曜日 ah時m分s秒", Locale.JAPANESE)) +"脱退させられた"
+			});
+
+			//通知
+			TextChannel CH = DISCORD_BOT.getTextChannelById("1128742498194444298");
+			if(CH != null){
+				CH.sendMessage( E.getGuild().getName().replace("@", "AD") + "から叩き出されました。。。\n" +
+						"これで" + DISCORD_BOT.getGuilds().size() + "個の鯖になりました").queue();
+			}
+		} catch (Exception EX) {
 			EX.printStackTrace();
 		}
 	}
