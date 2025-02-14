c3cadf1e082c6ea1daa95921b64137803880dd1c
SINtyanneru <irjitgd@gmail.com>
2025-02-14 11:15:40 +0900

    イコール

--- System/src/main/java/su/rumishistem/rumiabot/System/Discord/DiscordEventListener.java
+++ System/src/main/java/su/rumishistem/rumiabot/System/Discord/DiscordEventListener.java
@@ -38,7 +38,7 @@ public class DiscordEventListener extends ListenerAdapter {
 	public void onMessageReceived(MessageReceivedEvent E) {
 		//ブロック済みのユーザーなら此処で処理を中断する
 		if (!UserBlockCheck.isBlock(E.getAuthor().getId())) {
-			if (E.getMessage().getContentRaw().endsWith("ち") || E.getMessage().getContentRaw().endsWith("ちん") || E.getMessage().getContentRaw().endsWith("ま") || E.getMessage().getContentRaw().endsWith("まん")) {
+			if (E.getMessage().getContentRaw().equals("ち") || E.getMessage().getContentRaw().equals("ちん") || E.getMessage().getContentRaw().equals("ま") || E.getMessage().getContentRaw().equals("まん")) {
 				E.getMessage().reply("そういうのよくないと思うよ。").queue();
 			}
 
