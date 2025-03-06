5b801a871bfb302a3e0e2b7a03914eb9859f130c
SINtyanneru <irjitgd@gmail.com>
2025-03-06 13:26:51 +0900

    曜日が謎の言語になってたので修正

--- System/src/main/java/su/rumishistem/rumiabot/System/MODULE/DATE_FORMAT.java
+++ System/src/main/java/su/rumishistem/rumiabot/System/MODULE/DATE_FORMAT.java
@@ -2,23 +2,24 @@ package su.rumishistem.rumiabot.System.MODULE;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DATE_FORMAT {
 	public static String ZHUUNI_H(OffsetDateTime DATE) {
-		return DATE.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日E曜日 a hh時m分s秒"));
+		return DATE.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日E曜日 a hh時m分s秒").withLocale(Locale.JAPANESE));
 	}
 
 	public static String KOUKI(OffsetDateTime DATE) {
 		int NEN = Integer.parseInt(DATE.format(DateTimeFormatter.ofPattern("yyyy")));
 		NEN = NEN + 660;
 
-		return DATE.format(DateTimeFormatter.ofPattern(NEN + "年MM月dd日E曜日 a hh時m分s秒"));
+		return DATE.format(DateTimeFormatter.ofPattern(NEN + "年MM月dd日E曜日 a hh時m分s秒").withLocale(Locale.JAPANESE));
 	}
 
 	public static String KOUKI_SEIREKI(OffsetDateTime DATE) {
 		int SEIREKI = Integer.parseInt(DATE.format(DateTimeFormatter.ofPattern("yyyy")));
 		int KOUKI = SEIREKI + 660;
 
-		return DATE.format(DateTimeFormatter.ofPattern(KOUKI + "(" + SEIREKI + ")" + "年MM月dd日E曜日 a hh時m分s秒"));
+		return DATE.format(DateTimeFormatter.ofPattern(KOUKI + "(" + SEIREKI + ")" + "年MM月dd日E曜日 a hh時m分s秒").withLocale(Locale.JAPANESE));
 	}
 }
