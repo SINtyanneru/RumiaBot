68d5d276d1f1bd39062fb5e189f82eb0d18570b6
SINtyanneru <irjitgd@gmail.com>
2025-05-06 05:03:49 +0900

    ログとして吐かせろ

--- System/src/main/java/su/rumishistem/rumiabot/System/Telegram/TelegramBot.java
+++ System/src/main/java/su/rumishistem/rumiabot/System/Telegram/TelegramBot.java
@@ -1,6 +1,10 @@
 package su.rumishistem.rumiabot.System.Telegram;
 
 import static su.rumishistem.rumiabot.System.Main.SH;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import su.rumishistem.rumi_java_lib.SmartHTTP.HTTP_REQUEST;
 import su.rumishistem.rumi_java_lib.SmartHTTP.HTTP_RESULT;
 import su.rumishistem.rumi_java_lib.SmartHTTP.Type.EndpointFunction;
@@ -11,7 +15,10 @@ public class TelegramBot {
 		SH.SetRoute("/telegramwh", Method.POST, new EndpointFunction() {
 			@Override
 			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
-				System.out.println(r.GetEVENT().getPOST_DATA());
+				JsonNode Body = new ObjectMapper().readTree(r.GetEVENT().getPOST_DATA());
+
+				System.out.println(Body);
+
 				return new HTTP_RESULT(200, new byte[] {}, "text/plain");
 			}
 		});
