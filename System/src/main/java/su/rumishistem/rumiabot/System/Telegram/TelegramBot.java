d8dadd1b7971cb114ad4897ea6b226419a80a7aa
SINtyanneru <irjitgd@gmail.com>
2025-05-06 04:54:36 +0900

    あーれ？

--- System/src/main/java/su/rumishistem/rumiabot/System/Telegram/TelegramBot.java
+++ System/src/main/java/su/rumishistem/rumiabot/System/Telegram/TelegramBot.java
@@ -4,10 +4,11 @@ import static su.rumishistem.rumiabot.System.Main.SH;
 import su.rumishistem.rumi_java_lib.SmartHTTP.HTTP_REQUEST;
 import su.rumishistem.rumi_java_lib.SmartHTTP.HTTP_RESULT;
 import su.rumishistem.rumi_java_lib.SmartHTTP.Type.EndpointFunction;
+import su.rumishistem.rumi_java_lib.SmartHTTP.Type.EndpointEntrie.Method;
 
 public class TelegramBot {
 	public static void Main() {
-		SH.SetRoute("/telegramwh", new EndpointFunction() {
+		SH.SetRoute("/telegramwh", Method.POST, new EndpointFunction() {
 			@Override
 			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
 				System.out.println(r.GetEVENT().getPOST_DATA());
