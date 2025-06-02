60216f14ee6193dfef1b8d928d4b5a4738d09a32
SINtyanneru <irjitgd@gmail.com>
2025-06-03 02:03:39 +0900

    あ

--- Joke/src/main/java/su/rumishistem/rumiabot/Joke/Unko.java
+++ Joke/src/main/java/su/rumishistem/rumiabot/Joke/Unko.java
@@ -3,15 +3,34 @@ package su.rumishistem.rumiabot.Joke;
 import su.rumishistem.rumiabot.System.TYPE.ReceiveMessageEvent;
 
 public class Unko {
+	private static final int BaiBain = 5;
+
 	public static void Main(ReceiveMessageEvent e) {
 		String Content = e.GetMessage().GetText();
+		String ReturnContent = null;
 
 		if (Content.endsWith("う")) {
-			e.GetMessage().GetDiscordChannel().sendMessage("んこ").queue();
+			ReturnContent = "んこ";
+
+			if (e.GetMessage().GetDiscordGuild().getId().equals("1377631640662315080")) {
+				for (int I = 0; I < BaiBain; I++) {
+					ReturnContent += "\nんこ";
+				}
+			}
 		}
 
 		if (Content.endsWith("うん")) {
-			e.GetMessage().GetDiscordChannel().sendMessage("こ").queue();
+			ReturnContent = "こ";
+
+			if (e.GetMessage().GetDiscordGuild().getId().equals("1377631640662315080")) {
+				for (int I = 0; I < BaiBain; I++) {
+					ReturnContent += "\nこ";
+				}
+			}
+		}
+
+		if (ReturnContent != null) {
+			e.GetMessage().GetDiscordChannel().sendMessage(ReturnContent).queue();
 		}
 	}
 }
