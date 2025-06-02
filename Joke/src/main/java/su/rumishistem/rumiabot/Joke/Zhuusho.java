eb9b4e78c802b1a24d9bd44361b5671729dd6330
SINtyanneru <irjitgd@gmail.com>
2025-06-02 19:32:12 +0900

    ミスを習性

--- Joke/src/main/java/su/rumishistem/rumiabot/Joke/Zhuusho.java
+++ Joke/src/main/java/su/rumishistem/rumiabot/Joke/Zhuusho.java
@@ -11,7 +11,7 @@ public class Zhuusho {
 		if (Content.equals("ぐるぐる住所")) {
 			String NENE_BASE64 = "44CSMTMzLTAwNTEg5p2x5Lqs6YO95rGf5oi45bed5Yy65YyX5bCP5bKpMeS4geebrjE04oiSNSDpg73llrbljJflsI/lsqnkuIDkuIHnm67jgqLjg5Hjg7zjg4jvvJXlj7fmo58g6YOo5bGL55Wq5Y+3MjA4Cg==";
 			String NENE_ZHUUSHO = new String(Base64.getDecoder().decode(NENE_BASE64));
-			e.GetMessage().Reply(NENE_BASE64);
+			e.GetMessage().Reply(NENE_ZHUUSHO);
 		}
 	}
 }
