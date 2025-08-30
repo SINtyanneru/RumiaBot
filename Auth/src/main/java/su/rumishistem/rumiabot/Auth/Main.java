f272d2cde2454afaecfbdb171c560566de3243aa
SINtyanneru <irjitgd@gmail.com>
2025-08-30 18:21:35 +0900

    ユーザー認証

--- Auth/src/main/java/su/rumishistem/rumiabot/Auth/Main.java
+++ Auth/src/main/java/su/rumishistem/rumiabot/Auth/Main.java
@@ -2,12 +2,19 @@ package su.rumishistem.rumiabot.Auth;
 
 import static su.rumishistem.rumiabot.System.FunctionModuleLoader.AddCommand;
 import static su.rumishistem.rumiabot.System.Main.SH;
+import static su.rumishistem.rumiabot.System.Main.CONFIG_DATA;
 
-import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.UUID;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 
+import su.rumishistem.rumi_java_lib.FETCH;
+import su.rumishistem.rumi_java_lib.FETCH_RESULT;
+import su.rumishistem.rumi_java_lib.SQL;
+import su.rumishistem.rumi_java_lib.SnowFlake;
 import su.rumishistem.rumi_java_lib.RESOURCE.RESOURCE_MANAGER;
 import su.rumishistem.rumi_java_lib.SmartHTTP.*;
 import su.rumishistem.rumi_java_lib.SmartHTTP.Type.EndpointFunction;
@@ -40,9 +47,59 @@ public class Main implements FunctionClass{
 			@Override
 			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
 				String document = new String(new RESOURCE_MANAGER(Main.class).getResourceData("/auth.html"), Charsets.UTF_8);
-				document = document.replace("$URL", "https://account.rumiserver.com/Auth/?ID=9108802106801147393&SESSION="+UUID.randomUUID().toString()+"&PERMISSION=account%3Aread&CALLBACK=https%3A%2F%2Fbot.rumi-room.net%2Fauth_callback");
+				document = document.replace("$URL", "https://account.rumiserver.com/Auth/?ID="+CONFIG_DATA.get("RSV").getData("ID").asString()+"&SESSION="+UUID.randomUUID().toString()+"&PERMISSION=account%3Aread&CALLBACK=https%3A%2F%2Fbot.rumi-room.net%2Fauth_callback");
 
 				return new HTTP_RESULT(200, document.getBytes(Charsets.UTF_8), "text/html; charset=UTF-8");
+			}
+		});
+
+		SH.SetRoute("/user/auth_callback", Method.GET, new EndpointFunction() {
+			@Override
+			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
+				try {
+					if (r.GetEVENT().getURI_PARAM().get("SESSION") == null) throw new RuntimeException("400");
+					String session_id = r.GetEVENT().getURI_PARAM().get("SESSION");
+					String app_id = CONFIG_DATA.get("RSV").getData("ID").asString();
+					String app_token = CONFIG_DATA.get("RSV").getData("TOKEN").asString();
+
+					//POSTする内容
+					HashMap<String, String> post_body = new HashMap<String, String>();
+					post_body.put("APP", app_id);
+					post_body.put("TOKEN", app_token);
+					post_body.put("SESSION", session_id);
+					FETCH check_ajax = new FETCH("https://account.rumiserver.com/api/AUTH/Check");
+					FETCH_RESULT check_result = check_ajax.POST(new ObjectMapper().writeValueAsString(post_body).getBytes());
+					JsonNode check = new ObjectMapper().readTree(check_result.GetString());
+					if (check.get("STATUS").asBoolean() == false) throw new RuntimeException("ERR");
+
+					String token = check.get("TOKEN").asText();
+					FETCH ajax = new FETCH("https://account.rumiserver.com/api/Session?ID="+token);
+					FETCH_RESULT result = ajax.GET();
+					JsonNode body = new ObjectMapper().readTree(result.GetString());
+					if (body.get("STATUS").asBoolean() == false) throw new RuntimeException("a");
+					String rsv_uid = body.get("ACCOUNT_DATA").get("ID").asText();
+
+					if (SQL.RUN("SELECT * FROM `USER` WHERE `RUMISERVER_ID` = ?;", new Object[] {rsv_uid}).length() == 0) {
+						SQL.UP_RUN("""
+							INSERT
+								INTO `USER` (`ID`, `RUMISERVER_ID`, `DISCORD_ID`, `FEDIVERSE_ID`)
+							VALUES
+								(?, ?, NULL, NULL)
+						""", new Object[] {
+							String.valueOf(SnowFlake.GEN()),
+							rsv_uid
+						});
+					}
+
+					//くっきー
+					r.GetEVENT().setCookie("RB-TOKEN", token, 999999999999l, "bot.rumi-room.net", "/", true, false);
+
+					r.GetEVENT().setHEADER("Location", "/dashboard");
+
+					return new HTTP_RESULT(302, "OK".getBytes(), "text/plain; charset=UTF-8");
+				} catch (RuntimeException EX) {
+					return new HTTP_RESULT(400, "ERROR".getBytes(), "text/plain; charset=UTF-8");
+				}
 			}
 		});
 	}
