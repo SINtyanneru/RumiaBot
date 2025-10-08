56e4972c2f7adb3314f5e3338ec99e6aa66348c6
SINtyanneru <irjitgd@gmail.com>
2025-10-09 04:20:29 +0900

    少し改良

--- MusicPlayer/src/main/java/su/rumishistem/rumiabot/MusicPlayer/Main.java
+++ MusicPlayer/src/main/java/su/rumishistem/rumiabot/MusicPlayer/Main.java
@@ -2,42 +2,22 @@ package su.rumishistem.rumiabot.MusicPlayer;
 
 import static su.rumishistem.rumiabot.System.FunctionModuleLoader.AddCommand;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import java.io.*;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.regex.Pattern;
-
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
-
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.GuildVoiceState;
-import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
-import net.dv8tion.jda.api.managers.AudioManager;
-import su.rumishistem.rumiabot.System.TYPE.CommandData;
-import su.rumishistem.rumiabot.System.TYPE.CommandInteraction;
-import su.rumishistem.rumiabot.System.TYPE.CommandOption;
-import su.rumishistem.rumiabot.System.TYPE.CommandOptionType;
-import su.rumishistem.rumiabot.System.TYPE.FunctionClass;
-import su.rumishistem.rumiabot.System.TYPE.ReceiveMessageEvent;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import su.rumishistem.rumiabot.System.TYPE.*;
 
 public class Main implements FunctionClass {
 	private static final String FUNCTION_NAME = "音楽プレイヤー";
 	private static final String FUNCTION_VERSION = "1.0";
 	private static final String FUNCTION_AUTOR = "Rumisan";
+
+	private static HashMap<String, String> vc_id_table = new HashMap<>();
+	private static HashMap<String, Player> player_list = new HashMap<>();
 
 	@Override
 	public String FUNCTION_NAME() {
@@ -57,6 +37,9 @@ public class Main implements FunctionClass {
 		AddCommand(new CommandData("play", new CommandOption[] {
 			new CommandOption("url", CommandOptionType.String, null, true)
 		}, false));
+
+		AddCommand(new CommandData("stop", new CommandOption[] {}, false));
+		AddCommand(new CommandData("skip", new CommandOption[] {}, false));
 	}
 
 	@Override
@@ -69,9 +52,8 @@ public class Main implements FunctionClass {
 
 	@Override
 	public void RunCommand(CommandInteraction CI) throws Exception {
-		String url = CI.GetCommand().GetOption("url").GetValueAsString();
-		if (Pattern.compile("^(https?|ftp)://[\\w.-]+(?:\\.[\\w\\.-]+)+[/#?]?.*$").matcher(url).matches() == false) {
-			CI.Reply("URLが不正");
+		if (CI.GetSource() != SourceType.Discord) {
+			CI.Reply("Discordでのみ使用可能なコマンドです");
 			return;
 		}
 
@@ -87,84 +69,86 @@ public class Main implements FunctionClass {
 		GuildVoiceState voice_State = member.getVoiceState();
 		if (voice_State == null) throw new RuntimeException("ボイスチャンネル取得エラー");
 
+		//オーディオチャンネルを取得
 		AudioChannel vc = voice_State.getChannel();
 		if (vc == null) throw new RuntimeException("エラー");
 
-		AudioManager am = guild.getAudioManager();
+		Player player;
 
-		//LavaPlayer初期化
-		AudioPlayerManager apm = new DefaultAudioPlayerManager();
-		AudioSourceManagers.registerRemoteSources(apm);
-		AudioSourceManagers.registerLocalSource(apm);
-		AudioPlayer ap = apm.createPlayer();
+		if (vc_id_table.get(vc.getId()) == null) {
+			//プレイヤー作成
+			player = new Player(guild, vc);
+			player_list.put(player.get_id(), player);
+			vc_id_table.put(vc.getId(), player.get_id());
 
-		//VCに参加
-		am.setSendingHandler(new LavaPlayerSendHandler(ap));
-		am.openAudioConnection(vc);
+			CI.Reply("プレイヤー["+player.get_id()+"]を作成しました！");
+		} else {
+			player = player_list.get(vc_id_table.get(vc.getId()));
+			CI.Reply("プレイヤー["+player.get_id()+"]に命令...");
+		}
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					ProcessBuilder pb = new ProcessBuilder(new String[] {
-						"sh",
-						"-c",
-						"yt-dlp -f bestaudio --no-part -o - '"+url.replace("'", "")+"' | ffmpeg -i - -f mp3 -acodec libmp3lame -ar 44100 -ac 2 -"
-					});
-					Process p = pb.start();
+		switch (CI.GetCommand().GetName()) {
+			case "play":
+				play(CI.GetDiscordInteraction(), player);
+				return;
+			case "stop":
+				stop(CI.GetDiscordInteraction(), player, vc);
+				return;
+		}
+	}
 
+	private static void send_message(SlashCommandInteraction e, String text) {
+		e.getChannel().asTextChannel().sendMessage(text).queue();
+	}
 
-					File temp_file = new File("/tmp/" + UUID.randomUUID().toString());
-					temp_file.createNewFile();
-					FileOutputStream fos = new FileOutputStream(temp_file);
+	private static void play(SlashCommandInteraction e, Player player) throws IOException, InterruptedException {
+		send_message(e, "音声を取得しています、暫くお待ち下さい。");
 
-					//標準出力を読む
-					InputStream is = p.getInputStream();
-					byte[] buffer = new byte[8024];
-					int read_length = 0;
-					while ((read_length = is.read(buffer)) != -1) {
-						fos.write(buffer, 0, read_length);
-						fos.flush();
-					}
-					fos.close();
+		String url = e.getOption("url").getAsString();
+		if (Pattern.compile("^(https?|ftp)://[\\w.-]+(?:\\.[\\w\\.-]+)+[/#?]?.*$").matcher(url).matches() == false) {
+			e.getHook().editOriginal("URLが不正です").queue();
+			return;
+		}
 
-					int status = p.waitFor();
-					if (status != 0) {
-						temp_file.delete();
-						CI.Reply("Error:" + status);
-						return;
-					}
+		//yt-dlpから音声を落として、ffmpegにmp3化させる
+		ProcessBuilder pb = new ProcessBuilder(new String[] {
+			"sh",
+			"-c",
+			"yt-dlp -f bestaudio --no-part -o - '"+url.replace("'", "")+"' | ffmpeg -i - -f mp3 -acodec libmp3lame -ar 44100 -ac 2 -"
+		});
+		Process p = pb.start();
 
-					apm.loadItem(temp_file.toString(), new AudioLoadResultHandler() {
-						@Override
-						public void trackLoaded(AudioTrack track) {
-							ap.setVolume(50);
-							ap.playTrack(track);
+		//一時ファイルに書く
+		File temp_file = new File("/tmp/" + UUID.randomUUID().toString());
+		temp_file.createNewFile();
+		FileOutputStream fos = new FileOutputStream(temp_file);
 
-							ap.addListener(new AudioEventAdapter() {
-								@Override
-								public void onTrackEnd(AudioPlayer Player, AudioTrack Track, AudioTrackEndReason EndReason) {
-									temp_file.delete();
-								}
-							});
-						}
-						@Override
-						public void playlistLoaded(AudioPlaylist playlist) {}
-						@Override
-						public void noMatches() {}
-						@Override
-						public void loadFailed(FriendlyException exception) {}
-					});
-				} catch (IOException EX) {
-					EX.printStackTrace();
+		//標準出力を読む
+		InputStream is = p.getInputStream();
+		byte[] buffer = new byte[8024];
+		int read_length = 0;
+		while ((read_length = is.read(buffer)) != -1) {
+			fos.write(buffer, 0, read_length);
+			fos.flush();
+		}
+		fos.close();
 
-					try {
-						CI.Reply("エラー");
-					} catch (Exception e) {}
-				} catch (InterruptedException ex) {
-					ex.printStackTrace();
-				}
-			}
-		}).start();
+		//成功？
+		int status = p.waitFor();
+		if (status != 0) {
+			temp_file.delete();
+			send_message(e, "エラー！再生失敗");
+			return;
+		}
+
+		send_message(e, "終了、再生します。");
+
+		player.play(temp_file);
+	}
+
+	private static void stop(SlashCommandInteraction e, Player player, AudioChannel vc) throws IOException, InterruptedException {
+		player.stop();
+		player_list.remove(player.get_id());
+		vc_id_table.remove(vc.getId());
 	}
 }
